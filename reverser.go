17de4b998a2dfc49ebc1cc131d9f9cb943784c73
Robert Grandl <rgrandl@google.com>
2023-07-28 17:25:45 -0700

    upgrade 2

--- reverser.go
+++ reverser.go
@@ -40,13 +40,12 @@ type reverser struct {
 }
 
 func (r reverser) Reverse(ctx context.Context, s string) (string, error) {
-	return "hello everyone", nil
-//	stringLength.Put(float64(len(s))) // Update the stringLength metric.
-//
-//	runes := []rune(s)
-//	n := len(runes)
-//	for i := 0; i < n/2; i++ {
-//		runes[i], runes[n-i-1] = runes[n-i-1], runes[i]
-//	}
-//	return string(runes), nil
+	stringLength.Put(float64(len(s))) // Update the stringLength metric.
+
+	runes := []rune(s)
+	n := len(runes)
+	for i := 0; i < n/2; i++ {
+		runes[i], runes[n-i-1] = runes[n-i-1], runes[i-1]
+	}
+	return string(runes), nil
 }
